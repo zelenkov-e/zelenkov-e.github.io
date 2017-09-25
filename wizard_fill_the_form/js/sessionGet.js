14f7cf177aabc2bc53a5e53e807f9df8ba13082b
Zelenkov <zelenkov5111917@yandex.ru>
2017-09-25 12:43:08 +0300

    wizard_fill_form is deleted

--- wizard_fill_the_form/js/sessionGet.js
+++
@@ -1,7 +0,0 @@
-
-//вывод в новом документе
-var perem1 = sessionStorage.getItem('emailfield1');
-var perem2 = sessionStorage.getItem('emailfield2');
-var perem3 = sessionStorage.getItem('emailfield3');
-document.getElementById('result').innerHTML = "your name: " + perem1 + "; your surname: " + perem2 + "; your age: " + perem3;
-
