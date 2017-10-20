f8cb760e80f94125b4ff16c2117003ed10aa0518
Zelenkov <zelenkov5111917@yandex.ru>
2017-10-21 00:41:06 +0300

    deleted:    currency/

--- currency/js/currency.js
+++
@@ -1,7 +0,0 @@
-import { checkModule } from './rate_check.js'
-import { dateModule } from './rate_date.js'
-import { periodModule } from './rate_period.js'
-
-checkModule();
-dateModule();
-periodModule();
--- currency/js/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	entry:'./currency.js',
-	output:{
-		filename:'bundle.js',
-	}
-}
