73f99415c3cffa4262c3032370e0e7daab4bdcf1
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-01-31 17:37:49 +0300

    'modified:routing_webpack'

--- routing_webpack/app/components/home.jsx
+++ routing_webpack/app/components/home.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as pageActions from '../action/action';
+
+
  
-export default class Home extends React.Component{
+ class Home extends React.Component{
+
+    componentDidMount() {
+            this.props.actions.getNews()
+        }
+     
     render(){
+        const { newsElements } = this.props.news
         return (
+            
+
             <div>
-               <h3>home</h3>
+               <h3>{newsElements}</h3>
             </div>
         )
 
     }
 }
+
+const mapStateToProps = (state, ownProps) => ({
+    news: state.news 
+})
+
+
+// переедаем action - кот меняют состоянме в reducere
+function mapDispatchToProps(dispatch) {
+    return {
+       actions: bindActionCreators(pageActions, dispatch)
+    }
+}
+
+
+
+    export default connect(mapStateToProps,mapDispatchToProps) (Home);
---
+++ routing_webpack/app/reducers/infoNewsReduser.js
@@ -0,0 +1,24 @@
+import {
+    GET_NEWS_REQUEST,
+    GET_NEWS_SUCCESS
+   
+  } from "../constants/infoConstants";
+
+
+
+export default function newsInfo(state = [],action){
+    switch(action.type) {
+        case GET_NEWS_REQUEST:
+          return state;
+         
+        
+        case GET_NEWS_SUCCESS:
+          return  Object.assign(
+                {}, 
+                state,
+                //кон состояние - массив ajax 
+                { newsElements:action.payloadNews}
+            );
+         }
+        return state;
+      }
