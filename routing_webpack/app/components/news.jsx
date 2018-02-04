b4067e2800eeec773e555d4b751e5124b18e77ed
zelenkov-e <zelenkov5111917@yandex.ru>
2018-02-04 18:36:45 +0400

    'modified:routing_webpack'

--- routing_webpack/app/components/news.jsx
+++ routing_webpack/app/components/news.jsx
@@ -17,6 +17,10 @@ const Image = styled.img`
     margin-top:30px;
   
 `;
+const Title = styled.h4`
+   color:red;
+  
+`;
 
 
 
@@ -26,12 +30,13 @@ class News extends React.Component{
     showList() {
         
         return this.props.newsTitle.map((news,id,index,url,At)=>{
+         
             return (
                 <React.Fragment>
-                    <Image key = {news.url} src = { news.urlToImage } ></Image>
-                    <h4 key  = {news.id}>{news.title}</h4>
-                    <h6 key  = {news.At}>{news.publishedAt}</h6>
-                    <p key  = {news.index}>{news.description}</p>
+                 <Image key = {news.url} src = { news.urlToImage } ></Image>
+                 <Title key  = {news.id}>{news.title}</Title>
+                 <h6 key  = {news.At}>{news.publishedAt}</h6>
+                 <p key  = {news.index}>{news.description}</p>
 
                 </React.Fragment>
                 )
