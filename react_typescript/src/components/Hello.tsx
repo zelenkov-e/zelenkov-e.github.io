d9d068779a214722924607b75c2065c736720d0c
Zelenkov <zelenkov5111917@yandex.ru>
2018-02-12 21:49:33 +0200

    modified:   src/components/Hello.tsx

--- react_typescript/src/components/Hello.tsx
+++ react_typescript/src/components/Hello.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 
+// tslint:disable-next-line:interface-name
 export interface HelloProps { name: string; sername: string; }
 
 
 export class Hello extends React.Component<HelloProps, {}> {
-    render() {
+    public render() {
         return <h1>Hello  {this.props.name}  {this.props.sername}!</h1>;
     }
 }
--- react_typescript/src/index.tsx
+++ react_typescript/src/index.tsx
@@ -5,7 +5,6 @@ import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    // console.log('hi')
     <Hello name="Zhenya" sername="Zelenkof" />,
-    document.getElementById("example")
+    document.getElementById("example"),
 );
