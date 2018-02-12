b6be93a62a9b848b0f7866c9c2561daf9115eeec
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-12 09:14:38 +0300

    added tslint

--- react_typescript/src/components/Hello.tsx
+++ react_typescript/src/components/Hello.tsx
@@ -5,7 +5,6 @@ export interface HelloProps { name: string; sername: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-      
         return <h1>Hello  {this.props.name}  {this.props.sername}!</h1>;
     }
 }
