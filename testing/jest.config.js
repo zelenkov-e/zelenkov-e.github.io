17f5aca8ec56f07d553d9da3f4eabe499aff1b5d
Zelenkov <zelenkov5111917@yandex.ru>
2018-02-13 22:14:03 +0200

    new file:jest.config.js

---
+++ testing/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "moduleFileExtensions": [
+        "js",
+        "jsx"
+    ],
+    "moduleNameMapper": {
+        "Clock": "<rootDir>/app/_test_/components/Clock.jsx"
+        // "CountdownForm": "<rootDir>/app/components/CountdownForm.jsx",
+        // "Countdown": "<rootDir>/app/components/Countdown.jsx"
+    }
+}
