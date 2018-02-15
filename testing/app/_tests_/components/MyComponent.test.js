e86944cc2a9b50840bd1a0fb5486329b3b678ed6
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-15 10:12:04 +0300

    added MyComponent.test.js.snap

--- testing/app/_tests_/components/MyComponent.test.js
+++ testing/app/_tests_/components/MyComponent.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { MyComponent } from './MyComponent';
-import { shallowToJson  } from 'enzyme-to-json';
+// import { shallowToJson  } from 'enzyme-to-json';
 
 
 
 describe("MyComponent", () => {
-  it("should render my component", () => {
+  it(" render my component", () => {
     const wrapper = shallow(<MyComponent />);
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
