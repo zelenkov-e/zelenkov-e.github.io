ca87defeb390174f7cc715500a3f1e852fe4d07a
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-16 09:59:27 +0300

    modified file

--- routing_webpack/webpack.config.js
+++ routing_webpack/webpack.config.js
@@ -2,11 +2,13 @@ var path = require('path');
  
 module.exports = {
    entry: "./app/app.jsx", // входная точка - исходный файл
-   output:{
-       path: path.resolve(__dirname, './public/src'),     // путь к каталогу выходных файлов - папка public
-       publicPath: '/public',
-       filename: "bundle.js"       // название создаваемого файла
-   },
+//    output:{
+    //    path: path.resolve(__dirname, './public/src'),     // путь к каталогу выходных файлов - папка public
+    //    path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
+    //    publicPath: '/public',
+    //    filename: "bundle.js"       // название создаваемого файла
+//    },
+  output: { path: __dirname, filename: 'bundle.js'},
    devServer: {
      historyApiFallback: true,
    },
