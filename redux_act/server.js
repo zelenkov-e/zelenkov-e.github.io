acdff00d8a07af142ce23e863c089031543ed240
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-19 12:50:09 +0300

    added server.js

---
+++ redux_act/server.js
@@ -0,0 +1,32 @@
+const  path = require( "path" );
+const  express  = require( "express" );
+const  webpack  = require( "webpack" );
+const  webpackDevMiddleware  = require( "webpack-dev-middleware" );
+const  webpackHotMiddleware  = require( "webpack-hot-middleware" );
+const  config  = require( "./webpack.config.js" );
+
+const app = express(),
+      DIST_DIR = path.join(__dirname, "dist"),
+      PORT     = 3000,
+      compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+	publicPath: config.output.publicPath
+}));
+
+app.use(webpackHotMiddleware(compiler));
+
+app.get("*", (req, res, next) => {
+	const filename = path.join(DIST_DIR, "index.html");
+
+	compiler.outputFileSystem.readFile(filename, (err, result) => {
+	if (err) {
+		return next(err);
+	}
+	res.set('content-type', 'text/html');
+	res.send(result);
+	res.end();
+	});
+});
+
+app.listen(PORT);
