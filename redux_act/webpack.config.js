4905dec2d3599ca31503d62260e1128e73f2fe43
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-19 12:48:38 +0300

    modified webpack.config.js

--- redux_act/webpack.config.js
+++ redux_act/webpack.config.js
@@ -1,17 +1,43 @@
 var path = require('path');
+
+
+const webpack = require('webpack'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var DIST_DIR   = path.join(__dirname, "dist"),
+    CLIENT_DIR = path.join(__dirname, "src");
+
+
 module.exports = {
-  entry: './index.js',
-  output: { path: __dirname, filename: 'bundle.js'},
+  context: CLIENT_DIR,
+
+  entry: ["webpack-hot-middleware/client", "./main"],
+  output: { 
+    path: __dirname, 
+    publicPath: "/",
+    filename: 'bundle.js'
+  },
+
   module: {
       loaders: [
           { test: /\.js$/, 
             exclude: /node_modules/, 
-            loader: "babel", 
+            loader: "babel-loader", 
             query: 
             {
               presets:['react','es2015']}
             }
-  
       ]
-  }
+  },
+
+	resolve: {
+		extensions: ['.js']
+  },
+  
+  plugins: [
+		new HtmlWebpackPlugin({ template: 'index.html' }),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+	]
 };
