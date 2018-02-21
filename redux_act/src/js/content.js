a02afc0d720f1ceeddd92b826b45993ea2447e0b
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-02-21 17:47:46 +0300

    add reselect  in project

--- redux_act/src/js/content.js
+++ redux_act/src/js/content.js
@@ -1,41 +1,44 @@
-import React  from 'react';
-import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as pageActions from '../action/action';
-
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as pageActions from "../action/action";
+import { getStateProps } from "../selectors/selectors";
+import { createSelector } from "reselect";
 
 class Content extends React.Component {
-  
   componentDidMount() {
-    this.props.actions.increment()
-	console.log(this.props)
-}
-	
-	render() {
-		return (
-		  <div>
-			  	{/* <Button color="red" isHidden={true} /> */}
-				<h4>{this.props.element}</h4>
-				{/* <button onClick={() => this.props.actions.decrement(10)}>Do</button> */}
-			</div>
-		);
-	}
+    this.props.actions.increment();
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <Button color="red" isHidden={true} /> */}
+        <h4>{this.props.element}</h4>
+        {/* <button onClick={() => this.props.actions.decrement(10)}>Do</button> */}
+      </div>
+    );
+  }
 }
 
 // const Button = styled.button`
 // 	display: ${props => props.isHidden ? 'none' : 'block' };
 // 	color: ${props => props.color};
 // `;
-const mapStateProps = (state) => ({
-	// element: state.reducerAdd
-	element: state.reducerAdd
-	
-  })
-// переедаем action - кот меняют состоянме в reducere
-const mapDispatchToProps =  (dispatch)=> ({
-  actions: bindActionCreators(pageActions, dispatch)
-})
-// counterStore.dispatch(add);
 
-export default connect(mapStateProps,mapDispatchToProps)(Content);
+const mapStateProps = state => ({
+  // element: state.reducerAdd
+  element: getStateProps(state)
+});
+
+// переедаем action - кот меняют состоянме в reducere
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(pageActions, dispatch)
+});
+
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators({ ...pageActions }, dispatch);
+
+export default connect(mapStateProps, mapDispatchToProps)(Content);
 // export default connect(mapStateProps)(Content);
--- redux_act/webpack.config.js
+++ redux_act/webpack.config.js
@@ -1,47 +1,50 @@
-var path = require('path');
+var path = require("path");
 
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const webpack = require('webpack'); 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-var DIST_DIR   = path.join(__dirname, "dist"),
-    CLIENT_DIR = path.join(__dirname, "src");
-
+var DIST_DIR = path.join(__dirname, "dist"),
+  CLIENT_DIR = path.join(__dirname, "src");
 
 module.exports = {
-  devtool:"source-map",
+  devtool: "source-map",
   context: CLIENT_DIR,
 
   entry: ["webpack-hot-middleware/client", "./main"],
-  output: { 
-    path: __dirname, 
+  output: {
+    path: __dirname,
     publicPath: "/",
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
 
   module: {
-      loaders: [
-          { test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: "babel-loader", 
-            query: 
-            {
-              presets:['react','es2015']
-              // plugins: ["transform-object-rest-spread"]
-            
-            }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["react", "es2015", "stage-2"]
           }
-      ]
+        }
+        // loader: "babel-loader"
+        // query: {
+        // presets: ["react", "es2015", "transform-object-rest-spread"]
+        //   presets: ["react", "es2015", "stage-2"]
+        // }
+      }
+    ]
   },
 
-	resolve: {
-		extensions: ['.js']
+  resolve: {
+    extensions: [".js"]
   },
-  
+
   plugins: [
-		new HtmlWebpackPlugin({ template: 'index.html' }),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoEmitOnErrorsPlugin()
-	]
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
 };
