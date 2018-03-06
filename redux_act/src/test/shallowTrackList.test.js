639c540003a9b7771003926a5afb04e7766105cb
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-03-06 14:45:11 +0300

    added snapshots

--- redux_act/src/test/shallowTrackList.test.js
+++ redux_act/src/test/shallowTrackList.test.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { shallow } from "enzyme";
 import ComponentTrackList from "../js/componentTrackList";
-// import { ComponentShallow } from "./myCommponentTest";
 import { counterStore } from "../store/counterStore";
 
 describe("ComponentTrackList", () => {
   it(" render my component", () => {
     const wrapper = shallow(<ComponentTrackList store={counterStore} />);
-    // const wrapper = shallow(<ComponentShallow />);
     expect(wrapper).toMatchSnapshot();
   });
 });
