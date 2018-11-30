57e55f60c9f116b8d4e4a2f243cd5ea5e96cf0ae
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-11-30 14:55:17 +0300

    added  <h1> title for template

--- angularJS_test/src/main.js
+++ angularJS_test/src/main.js
@@ -5,7 +5,7 @@ var myApp = angular.module('myApp', []);
 myApp.directive("myDirective",function(){
   return{
     restrict:"E",
-    template:'<p>hello {{myCtrl.user}}</p>',
+    template:'<h1>hello {{myCtrl.user}}</h1>',
     controller: function(){
          this.user = 'Ivan'
     },
