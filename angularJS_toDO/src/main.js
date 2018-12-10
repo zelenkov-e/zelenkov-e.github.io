57d9a7ddfc2867ba8b82fc2b4893ed8a661b42be
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-12-10 11:36:48 +0300

    transfer logic appp from directive in controller

--- angularJS_toDO/src/main.js
+++ angularJS_toDO/src/main.js
@@ -2,34 +2,32 @@ import angular from 'angular';
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('listController', function() {
-  this.title = 'toDo';
-});
-
-myApp.directive('listDirective', () => {
+myApp.controller('listController', function($scope) {
   let LISTBUTTON = {
     add: 'add',
     delete: 'delete',
   };
+  $scope.title = 'toDo';
 
+  $scope.lists = LISTBUTTON;
+  $scope.tasks = [];
+  $scope.addElem = function(value) {
+    $scope.tasks.push(value);
+  };
+  $scope.clearAll = function() {
+    $scope.tasks = [];
+  };
+  $scope.deleteElem = function() {
+    $scope.task.splice(this.$index, 1);
+  };
+  $scope.getTotalTodos = function() {
+    return $scope.tasks.length;
+  };
+});
+
+myApp.directive('listDirective', () => {
   return {
     restrict: 'A',
     template: require('./template/listTemplate.html'),
-    link: function(scope) {
-      scope.lists = LISTBUTTON;
-      scope.tasks = [];
-      scope.addElem = function(value) {
-        scope.tasks.push(scope.value);
-      };
-      scope.clearAll = function() {
-        scope.tasks = [];
-      };
-      scope.deleteElem = function() {
-        scope.task.splice(this.$index, 1);
-      };
-      scope.getTotalTodos = function() {
-        return scope.tasks.length;
-      };
-    },
   };
 });
