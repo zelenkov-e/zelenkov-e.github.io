ae44d738175f44fc7765ae9c5ee246874e970446
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-12-10 16:13:36 +0300

    fixed inline editing

--- angularJS_inline_editor/src/main.js
+++ angularJS_inline_editor/src/main.js
@@ -1,44 +1,19 @@
 import angular from 'angular';
 
 var myApp = angular.module('myApp', []);
-// var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.directive('fooBar', () => {
-  var phones = [
-    {
-      name: 'Nokia Lumia 630',
-      year: 2014,
-      price: 200,
-      company: {
-        name: 'Nokia',
-        country: 'Финляндия',
-      },
-    },
-    {
-      name: 'Samsung Galaxy S 4',
-      year: 2014,
-      price: 400,
-      company: {
-        name: 'Samsung',
-        country: 'Республика Корея',
-      },
-    },
-    {
-      name: 'Mi 5',
-      year: 2015,
-      price: 300,
-      company: {
-        name: 'Xiaomi',
-        country: 'Китай',
-      },
-    },
-  ];
+myApp.controller('inlineEditorController', function($scope) {
+  $scope.value = ' Edit me.. ';
 
-  return {
-    restrict: 'E',
-    template: require('./template/bookmarks.html'),
-    link: function(scope) {
-      scope.phones = phones;
-    },
+  $scope.showtooltip = false;
+
+  $scope.hideTooltip = () => {
+    $scope.showtooltip = false;
+    console.log($scope.value);
+  };
+
+  $scope.toggleTooltip = event => {
+    event.stopPropagation();
+    $scope.showtooltip = !$scope.showtooltip;
   };
 });
