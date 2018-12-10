42c74266353108e7729e6c6c9569cbc7c2a10e89
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-12-10 14:05:24 +0300

    create simple temlate form for bulding shot application

--- angularJS_test/src/main.js
+++ angularJS_test/src/main.js
@@ -33,70 +33,12 @@ myApp.directive('fooBar', () => {
       },
     },
   ];
+
   return {
     restrict: 'E',
     template: require('./template/bookmarks.html'),
     link: function(scope) {
       scope.phones = phones;
     },
-  };
-});
-
-myApp.directive('formDirective', () => {
-  return {
-    restrict: 'E',
-    template: require('./template/template.html'),
-    controller: function($scope) {
-      this.user = {
-        message: 'USER',
-        text: 'fill out this form ..',
-        showElement: function(firstName, lastName) {
-          $scope.answer = `hello ${firstName} ${lastName}`;
-        },
-      };
-    },
-    controllerAs: 'myCtrl',
-  };
-});
-
-myApp.controller('scopeCtrl', $scope => {
-  $scope.name = 'HARRY';
-  console.log($scope);
-});
-
-myApp.directive('scopeDirective', () => {
-  return {
-    //незавимый scope в директиве ,
-    //отделяем scope ctrl от scope directive
-    //чтобы при баиндинге не изменить scope ctrl
-    scope: true,
-    //если  scope: false  - будет меняться scope  контроллера
-    restrict: 'E',
-    template: '<p> this directive -  {{name}} <input type="text" ng-model ="name"></p>',
-    link: function(scope) {
-      console.log(scope);
-    },
-  };
-});
-
-myApp.controller('callCtrl', $scope => {
-  $scope.name = 'Ivan Ivanov';
-  $scope.color = 'red';
-});
-
-myApp.directive('callDirective', () => {
-  return {
-    scope: {
-      //отделяем scope ctrl от scope directive
-      //только на чтение
-      name: '@',
-      //scope ctrl == scope directive
-      // 2 х сторонинний биндинг
-      color: '=',
-    },
-    restrict: 'E',
-    template:
-      '<p>{{name}}</p> <input type ="text" ng-model="name"/>' +
-      '<p>color: {{color}}</p> <input type="text" ng-model="color"  />',
   };
 });
