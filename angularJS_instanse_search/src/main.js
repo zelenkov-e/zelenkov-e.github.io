947f7f437069d783bc9bbfa7bab5fa5015e2e317
Yauheni Zeliankou <Yauheni_Zeliankou@epam.com>
2018-12-11 09:25:06 +0300

    new instanse seach  app

---
+++ angularJS_instanse_search/src/main.js
@@ -0,0 +1,43 @@
+import angular from 'angular';
+
+var myApp = angular.module('myApp', []);
+
+myApp.directive('fooBar', () => {
+  var phones = [
+    {
+      name: 'Nokia Lumia 630',
+      year: 2014,
+      price: 200,
+      company: {
+        name: 'Nokia',
+        country: 'Финляндия',
+      },
+    },
+    {
+      name: 'Samsung Galaxy S 4',
+      year: 2014,
+      price: 400,
+      company: {
+        name: 'Samsung',
+        country: 'Республика Корея',
+      },
+    },
+    {
+      name: 'Mi 5',
+      year: 2015,
+      price: 300,
+      company: {
+        name: 'Xiaomi',
+        country: 'Китай',
+      },
+    },
+  ];
+
+  return {
+    restrict: 'E',
+    template: require('./template/bookmarks.html'),
+    link: function(scope) {
+      scope.phones = phones;
+    },
+  };
+});
