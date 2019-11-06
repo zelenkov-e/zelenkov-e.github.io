4d81c7a320f4a9a6fe2f86edf2d819c4cf116860
Yauheni Zeliankoue <zelenkovzhenya@gmail.comm>
2019-11-06 17:35:40 +0300

    added Next.js

---
+++ hello-next/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Header from "../comps/Header";
+
+export default function Index() {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+      <p>this is basic page</p>
+      <Header />
+    </div>
+  );
+}
