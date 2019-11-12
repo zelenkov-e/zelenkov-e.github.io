19ee08cf95aa45adf8447433f0b4bb2657aa4071
Yauheni Zeliankoue <zelenkovzhenya@gmail.comm>
2019-11-12 11:29:39 +0300

    added stylized nested componetn

--- hello-next/pages/blog.js
+++ hello-next/pages/blog.js
@@ -1,9 +1,23 @@
 import Layout from "../comps/MyLayout";
 
+const Title = () => (
+  <div>
+    <h1>This is child component</h1>
+    <style jsx>
+      {`
+        h1 {
+          color: red;
+        }
+      `}
+    </style>
+  </div>
+);
+
 export default function Blog() {
   return (
     <Layout>
       <p>This is my blog</p>
+      <Title />
       <style jsx>
         {`
           p {
