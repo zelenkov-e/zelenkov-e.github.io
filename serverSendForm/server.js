c3e50c4b0ae76be14e24a01efeeff2594f389ada
Zelenkov <zelenkov5111917@yandex.ru>
2020-02-08 20:29:21 +0200

    fixed sending  massages form

--- serverSendForm/server.js
+++ serverSendForm/server.js
@@ -1,49 +1,76 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+var express = require('express');
+var bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 var app = express();
 app.use(bodyParser());
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/home.html");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/home.html');
 });
 
-app.get("/news", function(req, res) {
-  res.sendFile(__dirname + "/news.html");
-});
-
-app.post("/", function(req, res) {
+app.post('/', function(req, res) {
   var name = req.body.name;
   var email = req.body.email;
+  var phone = req.body.phone;
   var message = req.body.message;
-  var content = `name: ${name} \n email: ${email} \n message: ${content} `;
+  var content = `
+                <h1>У вас новое сообщение с сайта:</h1>
+                 <ul>
+                    <li>Клиент: ${name}</<li> 
+                    <li>почта: ${email}</<li> 
+                    <li>телефон: ${phone}</<li>
+                    <li>сообщение: ${message}</<li>
+                 </ul>
+                 `;
+  var response = `
+                <h1>У вас новое сообщение с сайта :</h1>
+                 <p>
+                    Сообщение получено
+                 </p>
+                 `;
 
   let transporter = nodemailer.createTransport({
-    service: "gmail",
+    //прослойка  - котор высылает письма
+    service: 'gmail',
     auth: {
-      user: "zelenkovzhenya@gmail.com",
-      pass: "20101986"
-    }
+      user: 'zelenkovzhenya@gmail.com',
+      pass: '****', //real password
+    },
   });
 
   let mailOptions = {
-    from: "zelenkovzhenya@gmail.com",
-    to: email,
-    subject: "Node contact request",
-    text: "Hello world?",
-    html: content
+    // from: email,
+    to: 'zelenkovzhenya@gmail.com', // куда будет приходить
+    subject: 'Письмо с сайта',
+    // text: 'Hello world?',
+    html: content,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
     } else {
-      console.log("Email sent: " + info.response);
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  let mailAnswerOptions = {
+    from: 'zelenkovzhenya@gmail.com',
+    to: email,
+    subject: 'Письмо с сайта Пямятники',
+    html: response,
+  };
+
+  transporter.sendMail(mailAnswerOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
     }
   });
 });
 
 app.listen(3000, function() {
-  console.log("listen port 3000");
+  console.log('listen port 3000');
 });
