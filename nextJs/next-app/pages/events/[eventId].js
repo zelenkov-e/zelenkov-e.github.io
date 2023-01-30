87eeeca7b270f03d8f8e757a5b3459157c1e4d0f
CORP\e_zelenkov <e_zelenkov@wargaming.net>
2023-01-30 13:03:36 +0300

    NEXT-APP added Event details page

--- nextJs/next-app/pages/events/[eventId].js
+++ nextJs/next-app/pages/events/[eventId].js
@@ -1,5 +1,27 @@
+import { useRouter } from 'next/router'
+import { Fragment } from 'react'
+import { getEventById } from '../../dummy-data'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventContent from '../../components/event-detail/event-content'
+import EventLogistics from '../../components/event-detail/event-logistics'
+
 function EventDatails() {
-    return <div>Event details</div>
+    const router = useRouter()
+    const event = getEventById(router.query.eventId)
+
+    if (!event) {
+        return <p>No event found !</p>
+    }
+
+    return (
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics event={event} />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
+    )
 }
 
 export default EventDatails
