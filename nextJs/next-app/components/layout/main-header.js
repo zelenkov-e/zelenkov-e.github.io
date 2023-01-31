350e83b5e831d5aee750a3d85eed6a6c01bea75f
CORP\e_zelenkov <e_zelenkov@wargaming.net>
2023-01-31 16:32:42 +0300

    NEXT-APP added home-icon to Main Header

--- nextJs/next-app/components/layout/main-header.js
+++ nextJs/next-app/components/layout/main-header.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import classes from './main-header.module.css'
-
+import HomeIcon from '../../icons/home-icon'
 
 function MainHeader() {
     return <header className={classes.header}>
-        <div className={classes.logo}>
-            <Link href='/'>Home</Link>
-        </div>
+        <Link href='/'>
+            <a className={classes.icon}>
+                <HomeIcon />
+            </a>
+        </Link>
         <nav className={classes.navigation}>
             <ul>
                 <li><Link href='/events'>events</Link></li>
