51ef6d19aafca52d08047e813bccb11eaa83cb64
CORP\e_zelenkov <e_zelenkov@wargaming.net>
2023-10-07 12:23:59 +0300

    [REACT-NATIVE-ROUTING] : added Drawer left panel

--- mobile/reactNative/Routing/App.tsx
+++ mobile/reactNative/Routing/App.tsx
@@ -1,22 +1,22 @@
-//tell React that we will implement a navigator
+import "react-native-gesture-handler";
+
 import { NavigationContainer } from "@react-navigation/native";
-//create a stack navigator
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import HomeScreen from "./components/Homescreen";
 import AboutScreen from "./components/Abouscreen";
 import ContactScreen from "./components/ContactScreen";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="Contact" component={ContactScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="Contact" component={ContactScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
